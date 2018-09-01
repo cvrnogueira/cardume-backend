b00d797abc2348ee912b6e2355e6691777fb5cab
gustavoterras <gustavo_terras@terceiros.sicredi.com.br>
2018-09-01 18:52:14 -0300

    firebase database integration.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,18 +1,39 @@
 package hello;
 
-import java.util.Arrays;
-
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        try {
+
+            FileInputStream serviceAccount = new FileInputStream("serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://cardume-2018.firebaseio.com/")
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        }catch (IOException e) {
+            System.out.println("Error");
+            //Log.error("error " + e);
+        }
     }
 
     @Bean
--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,15 +1,29 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class HelloController {
 
-    MockedData mocked = new MockedData();
-        @RequestMapping("/eventos")
-    public Evento index() {
-        return mocked.getEvent();
+    @RequestMapping("/eventos")
+    public Map<String, Object> index() throws ExecutionException, InterruptedException {
+
+        DocumentReference docRef = FirestoreClient.getFirestore().collection("cardume").document("eventos");
+
+        ApiFuture<DocumentSnapshot> future = docRef.get();
+
+        DocumentSnapshot document = future.get();
+
+        return document.getData();
     }
-    
+
 }
